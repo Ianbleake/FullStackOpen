a611c47d2781e29b7e429991e0884b7a47041b83
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-06-17 11:18:46 -0600

    Exercise 6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,14 +1,53 @@
 import { useState } from 'react'
 
+const Title = ({text})=>{
+  return(
+    <h1>{text}</h1>
+  )
+} 
+
+const Button = ({onClick,text})=>{
+  return(
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const Display = ({text,value}) =>{
+  return(
+    <p>{text}: {value}</p>
+  )
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const addGood = ()=>{
+    setGood(good+1)
+  }
+
+  const addNeutral = ()=>{
+    setNeutral(neutral+1)
+  }
+
+  const addBad=()=>{
+    setBad(bad+1)
+  }
+
   return (
     <div>
-      code here
+      <Title text='Give Feedback'/>
+      <Button onClick={addGood} text='Good' />
+      <Button onClick={addNeutral} text='Neutral' />
+      <Button onClick={addBad} text='Bad' />
+      <Title text='Statistics'/>
+      <Display text='Good' value={good} />
+      <Display text='Neutral' value={neutral} />
+      <Display text='Bad' value={bad} />
     </div>
   )
 }
