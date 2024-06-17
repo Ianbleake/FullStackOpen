b2db275a994be3d1c48781c7f51227a6fdca523e
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-06-17 11:05:59 -0600

    Exercise 6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,62 +1,14 @@
-const Header = (props)=>{
-  console.log(props)
-  return(<h1>{props.course.name}</h1>)
-}
-
-
-const Part = (props) => {
-  return(
-    <p>{props.title}: Num.exercises: {props.exercises}</p>
-  )
-}
-
-const Content = (props)=>{
-  console.log(props)
-  return(
-    <>
-    <Part title={props.course.parts[0].name} exercises={props.course.parts[0].exercises} />
-    <Part title={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
-    <Part title={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
-    </>
-  )
-}
-
-const Total = (props)=>{
-  let total = 0
-  let exercises = props.course.parts
-  exercises.forEach(element => {
-    total = total+element.exercises
-  });
-  return(<p>Total of exercises: {total}</p>)
-}
+import { useState } from 'react'
 
 const App = () => {
-
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course}/>
-
-      <Content course={course} />
-
-      <Total course={course}/>
+      code here
     </div>
   )
 }
