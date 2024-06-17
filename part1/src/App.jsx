ac755fd2ca5b05afbc9291202d0ae17304979715
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-06-17 11:55:01 -0600

    Exercise 9

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -21,17 +21,22 @@ const Display = ({ text, value }) => {
 }
 
 const Statistics = ({good,neutral,bad,all,average,percentage})=>{
-  return(
-    <div>
-      <Title text='Statistics'/>
-      <Display text='Good' value={good} />
-      <Display text='Neutral' value={neutral} />
-      <Display text='Bad' value={bad} />
-      <Display text='All' value={all} />
-      <Display text='Average' value={average} />
-      <Display text='Positive' value={`${percentage} %`} />
-    </div>
-  )
+  if(all == 0){
+    return(
+      <p>No Feedback given</p>
+    )
+  }else{
+    return(
+      <div>
+        <Display text='Good' value={good} />
+        <Display text='Neutral' value={neutral} />
+        <Display text='Bad' value={bad} />
+        <Display text='All' value={all} />
+        <Display text='Average' value={average} />
+        <Display text='Positive' value={`${percentage} %`} />
+      </div>
+    )
+  }
 }
 
 const App = () => {
@@ -64,6 +69,7 @@ const App = () => {
       <Button onClick={addGood} text='Good' />
       <Button onClick={addNeutral} text='Neutral' />
       <Button onClick={addBad} text='Bad' />
+      <Title text='Statistics'/>
       <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} percentage={positivePercentage} />
     </div>
   );
