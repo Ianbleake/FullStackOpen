01ecac57694e69eac9300e34a20053a4121087e5
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-06-17 11:42:54 -0600

    Exercise 7

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,55 +1,64 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-const Title = ({text})=>{
-  return(
+const Title = ({ text }) => {
+  return (
     <h1>{text}</h1>
-  )
-} 
+  );
+}
 
-const Button = ({onClick,text})=>{
-  return(
+const Button = ({ onClick, text }) => {
+  return (
     <button onClick={onClick}>
       {text}
     </button>
-  )
+  );
 }
 
-const Display = ({text,value}) =>{
-  return(
+const Display = ({ text, value }) => {
+  return (
     <p>{text}: {value}</p>
-  )
+  );
 }
 
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
 
-  const addGood = ()=>{
-    setGood(good+1)
+  const addGood = () => {
+    setGood(good + 1);
+    setAll(all + 1);
   }
 
-  const addNeutral = ()=>{
-    setNeutral(neutral+1)
+  const addNeutral = () => {
+    setNeutral(neutral + 1);
+    setAll(all + 1);
   }
 
-  const addBad=()=>{
-    setBad(bad+1)
+  const addBad = () => {
+    setBad(bad + 1);
+    setAll(all + 1);
   }
+
+  const average = all === 0 ? 0 : (good - bad) / all;
+  const positivePercentage = all === 0 ? 0 : (good * 100) / all;
 
   return (
     <div>
-      <Title text='Give Feedback'/>
+      <Title text='Give Feedback' />
       <Button onClick={addGood} text='Good' />
       <Button onClick={addNeutral} text='Neutral' />
       <Button onClick={addBad} text='Bad' />
-      <Title text='Statistics'/>
+      <Title text='Statistics' />
       <Display text='Good' value={good} />
       <Display text='Neutral' value={neutral} />
       <Display text='Bad' value={bad} />
+      <Display text='All' value={all} />
+      <Display text='Average' value={average} />
+      <Display text='Positive' value={`${positivePercentage} %`} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
