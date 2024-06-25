f4e42c19e616e54cd475bd2458bea765e34c8e17
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-06-25 12:53:09 -0600

    Exercise 2.2

--- part2/src/components/Content.jsx
+++ part2/src/components/Content.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Part from "./Part";
 
+const Content = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
 
-const Content = ({parts})=>{
-  return(
+  return (
     <ul>
-      {parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} id={part.id} /> )}
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} id={part.id} />
+      ))}
+      <li>Total Exercises: {total}</li>
     </ul>
-  )
-}
+  );
+};
 
 export default Content;
