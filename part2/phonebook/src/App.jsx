6aaf20861f165edf624105ef108e22a5a084adcf
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-06-25 16:03:37 -0600

    Exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,22 +2,33 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '0000000000' }
   ]) 
   const [newName, setNewName] = useState('Name')
+  const [newPhone, setNewPhone] = useState('0000000000')
 
-  const handleValue = (event)=>{
+  const handleName = (event)=>{
     setNewName(event.target.value)
+  }
+
+  const handlePhone = (event)=>{
+    setNewPhone(event.target.value)
   }
 
   const addContact = (event)=>{
     event.preventDefault()
     const contactObjet = {
-      name: newName
+      name: newName,
+      phone: newPhone
     }
-    if(persons.some(person => person.name === newName)){
-      alert(`${newName} Ya esta registrado`)
-      console.error('El nombre ',newName,' ya esta registrado')
+    if(persons.some(person => person.name === newName) || persons.some(person => person.phone === newPhone )){
+      if(persons.some(person => person.name === newName)){
+        alert(`${newName} Ya esta registrado`)
+        console.error('El nombre ',newName,' ya esta registrado')
+      }else{
+        alert(`${newPhone} Ya esta registrado`)
+        console.error('El numero: ',newPhone,' ya esta registrado')
+      }
     }else{
       setPersons(persons.concat(contactObjet))
       setNewName('')
@@ -29,7 +40,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input onChange={handleValue} value={newName} />
+          name: <input onChange={handleName} placeholder={newName} />
+        </div>
+        <div>
+          number: <input onChange={handlePhone} placeholder={newPhone} maxLength='10'/>
         </div>
         <div>
           <button onClick={addContact} type="submit">add</button>
@@ -37,7 +51,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person=> <li key={person.name} >{person.name}</li> )}
+        {persons.map(person=> <li key={person.name} >{person.name}: {person.phone}</li> )}
       </ul>
     </div>
   )
