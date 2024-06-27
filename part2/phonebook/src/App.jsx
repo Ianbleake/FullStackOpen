337f56da5c12315731d276571f5207a6c36fd894
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-06-27 16:12:58 -0600

    Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import { Title } from './components/Title'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('Name')
   const [newPhone, setNewPhone] = useState('0000000000')
   const [Search, setSearch] = useState('')
+
+  useEffect(() => {
+    console.log('Effect Avtive')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('Promise fulfilled');
+        setPersons(response.data)
+        console.log('Render:',persons.length, 'contacts')
+      })
+  }, []); 
 
   const handleName = (event)=>{
     setNewName(event.target.value)
