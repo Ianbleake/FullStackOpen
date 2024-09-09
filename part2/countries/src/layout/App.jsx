ccff3478c5fd569c65ec461b3a8b7daadf9222cc
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-09-09 11:37:44 -0600

    Exercise 2.19

--- part2/countries/src/layout/App.jsx
+++ part2/countries/src/layout/App.jsx
@@ -9,6 +9,13 @@ function App() {
   const [search, setSearch] = useState('');
   const [message, setMessage] = useState('');
   const [show, setShow] = useState(false);
+  const [showInfo,setShowInfo] = useState(false);
+  const [selected,setSelected] = useState(null);
+
+  const handleInfo = (countrie)=>{
+    setShowInfo(!showInfo);
+    setSelected(countrie);
+  }
 
   useEffect(() => {
     CountrieService.getAll().then(response => setCountries(response));
@@ -16,6 +23,10 @@ function App() {
 
   const handleSearch = (event) => {
     setSearch(event.target.value);
+    if(showInfo && selected !== null){
+      setSelected(null);
+      setShowInfo(false);
+    }
   };
 
   const filteredCountries = (countries || []).filter((countrie) => {
@@ -50,12 +61,13 @@ function App() {
       <div className='results'>
         {countries ? (
           <>
-            {filteredCountries.length > 1 && filteredCountries.length <= 10 ? (
+            {filteredCountries.length > 1 && filteredCountries.length <= 10 && !showInfo  ? (
               filteredCountries.map((countrie, index) => (
-                <div className='option' key={index}>{countrie.name.common}</div>
+                <div className='option' key={index}>{countrie.name.common} <button className='show' onClick={()=>handleInfo(countrie)} >Show</button></div>
+                
               ))
-            ) : filteredCountries.length === 1 ? (
-              <CountrieInfo info={filteredCountries[0]} />
+            ) : filteredCountries.length === 1 || showInfo ? (
+              <CountrieInfo info={ selected || filteredCountries[0]} />
             ) : null}
           </>
         ) : null}
