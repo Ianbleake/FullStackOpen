d31ba016cab712b619c5921aaa882016d707aa8f
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-09-10 11:14:47 -0600

    Exercise 3.5

--- part3/exercises/phonebook/index.js
+++ part3/exercises/phonebook/index.js
@@ -25,11 +25,22 @@ let Persons = [
   }
 ]
 
+const generateId = () => {
+  let newId;
+  do {
+    newId = Math.floor(Math.random() * 100) + 1;
+  } while (Persons.some(person => person.id === newId));
+  return newId;
+}
+
+//*Consultas
+
 app.get('/',(request,response)=>{
   response.send('<h1>Hello persons!</h1>')
 })
 
 app.get('/api/persons',(request,response)=>{
+  console.log("ID:",generateId())
   response.json(Persons)
 })
 
@@ -49,10 +60,30 @@ app.get('/info',(request,response)=>{
   response.send(`<h1>Phonebook has info for ${dataCount} people <br/> ${serverDate} </h1>`);
 })
 
+//*Eliminacion
 app.delete('/api/persons/:id',(request,response)=>{
   const id = Number(request.params.id);
   Persons = Persons.filter(person => person.id !== id);
   response.status(204).end();
+})
+
+//*Creacion
+
+app.post('/api/persons',(request,response)=>{
+  const body = request.body;
+
+  if(!body.name || !body.number){
+    return response.status(400).json({error: 'Content missing'})
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  Persons = Persons.concat(person);
+  response.json(person);
 })
 
 
