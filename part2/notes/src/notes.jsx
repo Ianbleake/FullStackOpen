004278fe41e9641494846953d4553e6953a938a4
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-09-10 12:48:41 -0600

    create notes app

---
+++ part2/notes/src/notes.jsx
@@ -0,0 +1,91 @@
+import { useState, useEffect } from 'react'
+import Note from './components/Note'
+import noteService from './services/notes'
+import { Title } from './components/Title'
+import MessageAlert from './components/MessageAlert'
+
+const App = () => {
+  const [notes, setNotes] = useState(null)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5,
+    }
+  
+    noteService
+      .create(noteObject)
+        .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService
+      .update(id, changedNote)
+        .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(() => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+  if(!notes){
+    return null;
+  }
+
+  return (
+    <div className='bdy'>
+      <Title text={'Notes'} />
+      <MessageAlert message={errorMessage} type={'error'} />
+      <div>
+        <button className='btn' onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>      
+      <ul className='tab'>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
+        )}
+      </ul>
+      <form className='frm' onSubmit={addNote}>
+      <input
+          className='inpt'
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button className='btnin' type="submit">save</button>
+      </form> 
+    </div>
+  )
+}
+
+export default App
