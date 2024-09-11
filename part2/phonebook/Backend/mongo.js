5be84522f6140f585bd48ab0c794ad5514c821d3
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-09-11 14:07:14 -0600

    Exercise 3.13

--- part2/phonebook/Backend/mongo.js
+++ part2/phonebook/Backend/mongo.js
@@ -26,7 +26,7 @@ if (process.argv.length===3) {
   console.log('phonebook:')
   Person.find({}).then(result => {
     result.forEach(person => {
-      console.log(person)
+      console.log(`${person.name} ${person.number}`)
     })
     mongoose.connection.close()
   })
