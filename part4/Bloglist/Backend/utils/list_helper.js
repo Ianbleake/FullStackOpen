1e381126a5b6bb761d6165dd3e36a2df41b418eb
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-09-12 17:57:23 -0600

    Exercise 4.4

--- part4/Bloglist/Backend/utils/list_helper.js
+++ part4/Bloglist/Backend/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1 ;
 }
 
+
+const totalLikes = (blogs)=>{
+  let count = 0;
+  blogs.map((blog) => {count = count+blog.likes})
+  return(count)
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
