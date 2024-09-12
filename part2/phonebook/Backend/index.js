d4bed1303ad38f6c98ed71c63c4a6cf24a3588fc
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-09-11 18:01:41 -0600

    Exercise 3.17

--- part2/phonebook/Backend/index.js
+++ part2/phonebook/Backend/index.js
@@ -84,6 +84,22 @@ app.post('/api/persons', (request, response) => {
   })
 });
 
+//*Actualizacion
+app.put('/api/persons/:id', (request, response, next)=>{
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 //*Midleware
 app.use(errorHandler)
 
