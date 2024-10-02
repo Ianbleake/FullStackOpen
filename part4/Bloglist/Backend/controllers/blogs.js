bb948f2830021e89ca6fd7371f5ac4d3a5d12809
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-10-02 11:58:05 -0600

    fix: add get users endpoint and blogs endpoint

--- part4/Bloglist/Backend/controllers/blogs.js
+++ part4/Bloglist/Backend/controllers/blogs.js
@@ -5,6 +5,7 @@ const User = require('../models/user')
 blogsRouter.get('/', async (request,response)=>{
   const blogs = await Blog
     .find({}).populate('user',{username: 1, name: 1})
+  response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
@@ -30,7 +31,8 @@ blogsRouter.post('/', async (request, response) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: user.id
   })
 
   const savedBlog = await blog.save()
