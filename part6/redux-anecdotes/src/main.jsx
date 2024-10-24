eabefb4c67571899222a99d65c29992c10f78f42
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-10-24 14:10:49 -0600

    Exercise 6.3

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import App from './App'
+import App from './Layout/App'
 import reducer from './reducers/anecdoteReducer'
 
 const store = createStore(reducer)
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,10 +1,11 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+const initialState = [
+  { id:1, content: 'If it hurts, do it more often', votes: 0 },
+  { id:2, content: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', votes: 0 },
+  { id:3, content: 'Adding manpower to a late software project makes it later!', votes: 0 },
+  { id:4, content: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', votes: 0 },
+  { id:5, content: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', votes: 0 },
+  { id:6, content: 'Premature optimization is the root of all evil.', votes: 0 },
+  { id:7, content: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', votes: 0 }
 ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
@@ -17,11 +18,32 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+export const vote = (id) => {
+  return {
+    type: 'VOTE',
+    data: {
+      id: id
+    }
+  }
+}
+
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+
+  console.log('Action:',action)
+  
+  switch(action.type){
+    case 'VOTE': {
+      const id = action.data.id
+      const anecdote = state.find(a => a.id === id )
+      const updateAnecdote = {
+        ...anecdote,
+        votes: anecdote.votes + 1
+      }
+      return state.map(story => story.id !== id ? story : updateAnecdote )
+    }
+
+  }
 
   return state
 }
