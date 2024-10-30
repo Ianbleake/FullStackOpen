7f4a01f4d781c21093f9b6a17f0e0e8712cc8cab
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-10-30 17:06:43 -0600

    Exercise 6.15

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,15 +1,19 @@
 import { useDispatch } from 'react-redux'
 import { create } from '../reducers/anecdoteReducer'
-import { addAlert, clearAlerts } from '../reducers/alertReducer';
+import { addAlert, clearAlerts } from '../reducers/alertReducer'
+import anecdoteService from '../Services/anecdotes'
 
 const AnecdoteForm = () => {
 
   const dispatch = useDispatch()
 
-  const handleSubmit = (event)=>{
+  const handleSubmit = async (event)=>{
     event.preventDefault()
+    const newStory = await anecdoteService.createNew(event.target.story.value)
     dispatch(create(event.target.story.value))
     event.target.story.value = ''
+    console.log(newStory)
+    
     dispatch(addAlert({ text: 'Your anecdote was added successfully!', type: 'success' }));
     setTimeout(() => {
       dispatch(clearAlerts()); 
