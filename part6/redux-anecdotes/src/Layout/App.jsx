3316f2400af6ea2fac77897a8107d245b098983f
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-10-30 16:52:16 -0600

    Exercise 6.14

--- part6/redux-anecdotes/src/Layout/App.jsx
+++ part6/redux-anecdotes/src/Layout/App.jsx
@@ -1,10 +1,22 @@
+import { useDispatch } from 'react-redux'
 import AnecdoteForm from '../components/AnecdoteForm'
 import AnecdoteList from '../components/AnecdoteList'
 import Filter from  '../components/Filter'
 import Notification from '../components/Notification'
 import '../Styles/App.css'
+import { useEffect } from 'react'
+import { setAnecdotes } from '../reducers/anecdoteReducer'
+import anecdoteService from '../Services/anecdotes'
+
 
 const App = () => {
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+        .then(a => dispatch(setAnecdotes(a)))
+  },[dispatch])
 
   return (
     <div className='card shadow' >
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,20 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const initialState = [
-  { id:1, content: 'If it hurts, do it more often', votes: 0 },
-  { id:2, content: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', votes: 0 },
-  { id:3, content: 'Adding manpower to a late software project makes it later!', votes: 0 },
-  { id:4, content: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', votes: 0 },
-  { id:5, content: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', votes: 0 },
-  { id:6, content: 'Premature optimization is the root of all evil.', votes: 0 },
-  { id:7, content: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', votes: 0 }
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
     create: (state,action) => {
       const content = action.payload;
@@ -31,10 +21,16 @@ const anecdoteSlice = createSlice({
       if(anecdote){
         anecdote.votes += 1;
       }
+    },
+    appendAndecdote: (state,action)=>{
+      state.push(action.payload)
+    },
+    setAnecdotes(state,action){
+      return action.payload
     }
   }
 
 })
 
-export const { vote, create } = anecdoteSlice.actions;
+export const { vote, create, appendAndecdote, setAnecdotes } = anecdoteSlice.actions;
 export default anecdoteSlice.reducer;
